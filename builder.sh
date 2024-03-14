d08f6bde82cdb63f7aba14d2d4451f7cfcd16884
ZestyCardboard <59447156+ZestyCardboard@users.noreply.github.com>
2024-03-14 01:14:42 -0400

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -24,7 +24,7 @@ apt install gcc -y
 apt install libgmp3-dev -y
 apt install pkg-config -y
 #configure strongswan
-./configure --prefix=/usr --sysconfdir=/etc  --enable-charon  --enable-ikev2 --enable-vici --enable-swanctl  --enable-x509 
+./configure --prefix=/usr --sysconfdir=/etc --enable-curve25519 --enable-systemd --enable-swanctl --with-systemdsystemunitdir=/lib/systemd/system --with-systemduserunitdir=/usr/lib/systemd 
 #make strongswan
 apt install make
 #put build-essentials here (Geoff)
