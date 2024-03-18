6e92404649600029cdc3aacd10116e286efce00d
ZestyCardboard <59447156+ZestyCardboard@users.noreply.github.com>
2024-03-18 15:37:08 -0400

    deploy failed, more manual approach required for now

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,3 @@ cd /root
 git clone https://github.com/phl4r3/wisp-ikev2
 cd wisp-ikev2
 chmod +x ./builder.sh
-./builder.sh
-./configureMoon.sh
-wall "Builder Complete, continue to configuration"
--- deploySun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /root
-git clone https://github.com/phl4r3/wisp-ikev2
-cd wisp-ikev2
-chmod +x ./builder.sh
-./builder.sh
-./configureSun.sh
-wall "Builder Complete, continue to configuration"
