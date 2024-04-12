91db8f173f80482f58becadeb63b0ffab015611b
Geoff Twardokus <geoff.twardokus@mail.rit.edu>
2024-04-12 10:22:24 -0400

    Update builder with tweaks for local tests

--- builder.sh
+++ builder.sh
@@ -6,9 +6,14 @@ if [ $(id -u) -ne 0 ]; then
   exit 1
 fi
 
+REPOLOCATION=$(pwd)
+
+cd ~
+
 #make logging directory
 mkdir logging
 touch logging/logpq.txt
+
 #install preq for making strongswan and liboqs and packet dropper
 apt install pip -y
 pip install scapy
@@ -18,9 +23,11 @@ apt install pkg-config -y
 apt install libsystemd-dev -y
 apt install libssl-dev -y
 apt install iproute2 iputils-ping nano wget unzip bzip2 make gcc libssl-dev cmake ninja-build -y
+
+cd ~
 #install liboqs
-mkdir /liboqs 
-cd /liboqs 
+mkdir liboqs 
+cd liboqs 
 wget https://github.com/open-quantum-safe/liboqs/archive/refs/tags/0.9.2.zip 
 unzip 0.9.2.zip 
 cd liboqs-0.9.2 
@@ -28,14 +35,16 @@ mkdir build && cd build
 cmake -GNinja -DOQS_USE_OPENSSL=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/usr \
                 -DCMAKE_BUILD_TYPE=Release -DOQS_BUILD_ONLY_LIB=ON .. 
 ninja && ninja install 
-cd / && rm -R /liboqs 
+cd ~ && rm -R liboqs 
+
 #cd back to working space
-cd /root/wisp-ikev2/
+cd $REPOLOCATION
 #make scripts runable
 chmod +x configureMoon.sh
 chmod +x configureSun.sh
 chmod +x forwardData.sh
 chmod 600 id_rsa
+
 #get strongswan from download and unzip
 wget https://download.strongswan.org/strongswan-6.0.0beta6.tar.bz2
 tar -jxvf strongswan-6.0.0beta6.tar.bz2
@@ -53,7 +62,6 @@ cd strongswan-6.0.0beta6
 ./configure --enable-cmd --enable-conftest --enable-counters --enable-openssl --enable-systemd --with-systemdsystemunitdir=/lib/systemd/system --enable-acert --enable-files --enable-swanctl --disable-charon --disable-stroke --disable-scepclient --disable-ikev1 --enable-frodo --enable-oqs --enable-silent-rules --sysconfdir=/etc
 #make strongswan
 apt install make
-#put build-essentials here (Geoff)
 make
 make install
 
