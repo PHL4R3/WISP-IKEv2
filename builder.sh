fa4b34e11e0305ce0d734a5b83f439f26ffaab5c
William Joslin <60491197+wpj3799@users.noreply.github.com>
2024-04-12 19:06:52 -0400

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -36,21 +36,15 @@ chmod +x configureMoon.sh
 chmod +x configureSun.sh
 chmod +x forwardData.sh
 chmod 600 id_rsa
-#get strongswan from download and unzip
-wget https://download.strongswan.org/strongswan-6.0.0beta6.tar.bz2
-tar -jxvf strongswan-6.0.0beta6.tar.bz2
-while [! -d "strongswan-6.0.0beta6"]; do
-    echo "waiting for tar to unzip"
-    sleep 1
-done
-
-rm strongswan-6.0.0beta6/src/swanctl/commands/rekey.c
-cp rekey.c strongswan-6.0.0beta6/src/swanctl/commands/
-cd strongswan-6.0.0beta6
-
+#get strongswan from git
+mkdir strongswan
+cd strongswan
+git clone https://github.com/wpj3799/StrongSwan-PQ/tree/RFC-9329---TCP
+rm /src/swanctl/commands/rekey.c
+cp rekey.c /src/swanctl/commands/
 
 #configure strongswan
-./configure --enable-cmd --enable-conftest --enable-counters --enable-openssl --enable-systemd --with-systemdsystemunitdir=/lib/systemd/system --enable-acert --enable-files --enable-swanctl --disable-charon --disable-stroke --disable-scepclient --disable-ikev1 --enable-frodo --enable-oqs --enable-silent-rules --sysconfdir=/etc
+./configure --enable-tcpd --disable-socket-default --enable-cmd --enable-conftest --enable-counters --enable-openssl --enable-systemd --with-systemdsystemunitdir=/lib/systemd/system --enable-acert --enable-files --enable-swanctl --disable-charon --disable-stroke --disable-scepclient --disable-ikev1 --enable-frodo --enable-oqs --enable-silent-rules --sysconfdir=/etc
 #make strongswan
 apt install make
 #put build-essentials here (Geoff)
